cae222022e1ae059907f8a2348032b202236bc97
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-04-21 19:30:42 +0530

    set up the auth service

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,8 +1,44 @@
 import {betterAuth} from "better-auth"
+import { jwt } from "better-auth/plugins"
 import { mongoClientDb } from "./index"
 import { mongodbAdapter } from "@better-auth/mongo-adapter"
 
 export const auth = betterAuth({
-  database: mongodbAdapter(mongoClientDb())
-
+  database: mongodbAdapter(mongoClientDb()),
+  emailAndPassword: {
+    enabled: true
+  },
+  //social providers : {google, facebook}
+  user: {
+    additionalFields: {
+      role: {
+        type: ["user", "manager", "admin"],
+        required: false,
+        defaultValue: "user",
+        input: false
+      }
+    }
+  },
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  plugins: [
+    jwt({
+      jwt: {
+        definePayload: ({user}) => {
+          return {
+            id: user.id,
+            email: user.email,
+            role: user.role
+          }
+        }
+      },
+      jwks: {
+        keyPairConfig: {
+          alg: "EdDSA",
+          crv: "Ed25519"
+        },
+        rotationInterval: 60 * 60 * 24 * 7
+      }
+    })
+  ]
 })
