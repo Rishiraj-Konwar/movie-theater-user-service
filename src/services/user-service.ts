9e89a6198cfb53237d52ae50d4d4f689285d88a2
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-04-30 23:16:45 +0530

    Add app error class

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -2,9 +2,10 @@ import type { IUserRepository } from "../types";
 
 export class UserService {
   private userRepo: IUserRepository
-  constructor(userRepo: IUserRepository){
+  constructor(userRepo: IUserRepository){ //using dependency injection here. Practising the Strategy pattern
     this.userRepo = userRepo
   }
 
+  
   
 }
