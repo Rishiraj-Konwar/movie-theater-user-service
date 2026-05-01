12eb6c5c6452b9ab3342570e22aa62b1bc7a8552
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-05-02 00:32:13 +0530

    Add the user controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -13,22 +13,32 @@ export function userController(userRepo: IUserRepository) {
       const user = await userService.getUser(id);
       SuccessResponse.data = { user: user };
       res.status(StatusCodes.OK).json(SuccessResponse);
-      return
+      return;
     }),
     getUserByEmail: ucTryCatch(async (req: Request, res: Response) => {
-      const email = req.headers["x-user-email"] as string
-      const user = await userService.getUserByEmail(email)
-      SuccessResponse.data = {user: user}
-      res.status(StatusCodes.OK).json(SuccessResponse)
-      return
+      const email = req.headers["x-user-email"] as string;
+      const user = await userService.getUserByEmail(email);
+      SuccessResponse.data = { user: user };
+      res.status(StatusCodes.OK).json(SuccessResponse);
+      return;
     }),
     updateUser: ucTryCatch(async (req: Request, res: Response) => {
-      const id = req.headers["x-user-id"] as string
-      const data: Partial<UserDoc> = req.body
-      const updatedUser = await userService.updateUser(id, data)
-      SuccessResponse.data = {user: updatedUser}
-      res.status(StatusCodes.OK).json(SuccessResponse)
-      return
-    })
+      const id = req.headers["x-user-id"] as string;
+      const data: Partial<UserDoc> = req.body;
+      const updatedUser = await userService.updateUser(id, data);
+      SuccessResponse.data = { user: updatedUser };
+      res.status(StatusCodes.OK).json(SuccessResponse);
+      return;
+    }),
+    deleteUser: ucTryCatch(async (req: Request, res: Response) => {
+      const id = req.headers["x-user-id"] as string;
+      const deletedUser = (await userService.deleteUser(id)) as UserDoc;
+      SuccessResponse.data = {
+        message: `User ${deletedUser.name} is deleted`,
+        user: deletedUser,
+      };
+      res.status(StatusCodes.OK).json(SuccessResponse);
+      return;
+    }),
   };
 }
