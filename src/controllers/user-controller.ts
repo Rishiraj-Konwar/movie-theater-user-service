87fe9a2cd66bddebbe81fbb117c132c9bd41a365
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-05-01 23:49:03 +0530

    refactoring

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,3 +1,19 @@
-import { UserService } from '../services/user-service';
-import type { Request, Response } from 'express';
+import { UserService } from "../services/user-service";
+import type { Request, Response } from "express";
+import { ucTryCatch } from "../utils/try-catch";
+import type { IUserRepository } from "../types";
+import { SuccessResponse } from "../utils";
+import { StatusCodes } from "http-status-codes";
 
+function userController(userRepo: IUserRepository) {
+  const userService = UserService(userRepo);
+  return {
+    getUser: ucTryCatch(async (req: Request, res: Response) => {
+      const id = req.headers["x-user-id"] as string;
+      const user = await userService.getUser(id);
+      SuccessResponse.data = { user: user };
+      res.status(StatusCodes.OK).json(SuccessResponse);
+    }),
+    
+  };
+}
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -2,24 +2,24 @@ import type { IUserRepository, UserDoc } from "../types";
 import { AppError } from "../utils";
 import { usTryCatch } from "../utils";
 //using dependency injection here. Practising the Strategy pattern
-export async function UserService(userRepo: IUserRepository) {
+export function UserService(userRepo: IUserRepository) {
   return {
-    getUser(id: string): Promise<UserDoc | AppError> {
+    async getUser(id: string): Promise<UserDoc | AppError> {
       return usTryCatch(() => userRepo.getUser(id));
     },
 
-    getUserByEmail(email: string): Promise<UserDoc | AppError> {
+    async getUserByEmail(email: string): Promise<UserDoc | AppError> {
       return usTryCatch(() => userRepo.getUserByEmail(email));
     },
 
-    updateUser(
+    async updateUser(
       id: string,
       data: Partial<UserDoc>,
     ): Promise<UserDoc | AppError> {
       return usTryCatch(() => userRepo.updateUser(id, data));
     },
 
-    deleteUser(id: string): Promise<UserDoc | AppError> {
+    async deleteUser(id: string): Promise<UserDoc | AppError> {
       return usTryCatch(() => userRepo.deleteUser(id));
     },
   };
