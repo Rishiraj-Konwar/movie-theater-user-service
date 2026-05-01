7669d01e04e8d8071a34d847e7a85007c003f7cc
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-05-01 23:13:50 +0530

    Add the controller try catch logic

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,12 +1,3 @@
 import { UserService } from '../services/user-service';
 import type { Request, Response } from 'express';
-export class UserController {
-  private userService: UserService
-  constructor(userService: UserService){
-    this.userService = userService
-  }
-  async getUser(req: Request, res: Response){
-    const id = req.headers["x-user-id"] as string
-    const response = await this.userService.getUser(id)
-  }
-}
+
--- src/utils/try-catch.ts
+++ src/utils/try-catch.ts
@@ -1,7 +1,8 @@
 import type { UserDoc } from "../types"
 import { AppError } from "./app-error"
 import { StatusCodes } from "http-status-codes"
-
+import type {Request, Response, NextFunction, RequestHandler} from "express"
+import { ErrorResponse } from "./error-response"
 
 export async function usTryCatch(task: () => Promise<UserDoc | null>): Promise<UserDoc | AppError>{
     try{
@@ -14,4 +15,20 @@ export async function usTryCatch(task: () => Promise<UserDoc | null>): Promise<U
       if (err instanceof AppError) throw err
       throw new AppError("Internal Server Error", StatusCodes.INTERNAL_SERVER_ERROR)
     }
+}
+
+export function ucTryCatch(task: RequestHandler): RequestHandler{
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try{
+      await task(req, res, next)
+    }catch(err){
+      if(err instanceof AppError){
+        ErrorResponse.error = {explanation: err.message}
+        res.status(err.statusCode).json(ErrorResponse)
+        return
+      }
+      ErrorResponse.error = {explanation: "Internal server error"}
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse)
+    }
   }
+}
